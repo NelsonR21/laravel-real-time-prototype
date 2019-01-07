7227562f2a34b329ca7ef140c494d41b95fd681b
Nelson Rodriguez <nelsonrodriguezj@gmail.com>
2019-01-07 18:36:13 -0400

    Config to broadcast added

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,6 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
+Broadcast::routes();
 Route::get('/messages', 'MessageController@fetch');
 Route::post('/messages', 'MessageController@sentMessage');
