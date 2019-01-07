a4369acbe5caebf81778b60f51e19a657a51b3a4
Nelson Rodriguez <nelsonrodriguezj@gmail.com>
2019-01-07 14:27:06 -0400

    Added methods and routes to MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use App\Events\NewMessage;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+
 
 class MessageController extends Controller
 {
     public function fetch ()
     {
-        return Message::all();
+        return Message::paginate(40);
+    }
+
+    public function sentMessage (Request $request)
+    {
+        $message = Message::create([
+            'message' => $request->message,
+            'date' => Carbon::now()
+        ]);
+        event(new NewMessage($message));
     }
 }
